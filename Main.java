be7f5b3e2b9619b36ae3e2a3c8f428c7ff62a40b
MaximDesyatkov <78271197+MaximDesyatkov@users.noreply.github.com>
2021-08-13 19:13:39 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Workflow workflow = new Workflow();
 
-        workflow.run("C:\\Users\\User\\IdeaProjects\\Lab2\\src\\input.txt");
+        workflow.run("C:\\Users\\User\\IdeaProjects\\Lab2\\input.txt");
     }
 }
