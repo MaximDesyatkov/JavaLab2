56aa4603c423ff4803174b6bb28f01794dfcb7ef
MaximDesyatkov <78271197+MaximDesyatkov@users.noreply.github.com>
2021-08-13 20:06:31 +0700

    Update Factory.java

--- Factory.java
+++ Factory.java
@@ -1,1 +1,37 @@
+import blocks.Operation;
 
+import java.io.IOException;
+import java.util.Properties;
+
+public class Factory {
+    private final Properties properties = new Properties();
+    private static Factory factory = null;
+
+    private Factory() throws IOException {
+        var input = Factory.class.getResourceAsStream("inputOperations");
+
+        if (input != null) {
+            properties.load(input);
+            input.close();
+        }
+    }
+
+    public static Factory getInstance() throws IOException {
+        if (factory == null) {
+            synchronized (Factory.class){
+                if(factory == null){
+                    factory = new Factory();
+                }
+            }
+        }
+        return factory;
+    }
+
+    public Operation createOperation(String operationName) throws Exception {
+        Operation currentOperation;
+        var classOfOperation = Class.forName(properties.getProperty(operationName));
+        var objectInstance = classOfOperation.getDeclaredConstructor().newInstance();
+        currentOperation = (Operation) objectInstance;
+        return currentOperation;
+    }
+}
