83a53ecc36f162ad34e1204b999556a0e6487cea
MaximDesyatkov <78271197+MaximDesyatkov@users.noreply.github.com>
2021-08-13 19:12:45 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Workflow workflow = new Workflow();
+
+        workflow.run("C:\\Users\\User\\IdeaProjects\\Lab2\\src\\input.txt");
+    }
+}
