d598f9997123893e7c7825a590905b4216078199
MaximDesyatkov <78271197+MaximDesyatkov@users.noreply.github.com>
2021-08-15 17:06:50 +0700

    Update Sort.java

--- Blocks/Sort.java
+++ Blocks/Sort.java
@@ -1,1 +1,20 @@
+package blocks;
 
+import exception.*;
+
+import java.util.Vector;
+import java.util.ArrayList;
+
+public class Sort implements Operation {
+    public void execute(ArrayList<String> text, Vector<String> args) throws WrongNumberOfArgs {
+        if (args.size() != 0){
+            throw new WrongNumberOfArgs("Only 1 argument expected");
+        }
+
+        text.sort((arg1, arg2) -> arg1.compareToIgnoreCase(arg2));
+    }
+
+    public OperationTypes getType() {
+        return OperationTypes.IN_OUT;
+    }
+}
