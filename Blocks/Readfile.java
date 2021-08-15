631f015285eaa50a67900c45aa751cbf6c23a82b
MaximDesyatkov <78271197+MaximDesyatkov@users.noreply.github.com>
2021-08-15 17:01:25 +0700

    Update Readfile.java

--- Blocks/Readfile.java
+++ Blocks/Readfile.java
@@ -1,1 +1,35 @@
+package blocks;
 
+import exception.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Readfile implements Operation {
+    public OperationTypes getType(){
+        return OperationTypes.IN;
+    }
+    public void execute(ArrayList<String> text, Vector<String> args) throws FileNotFoundException, WrongNumberOfArgs {
+
+        if (args.size() - 1 != 1){
+            throw new WrongNumberOfArgs("Only 1 argument expected");
+        }
+
+        String currentFile = args.get(0);
+        String currentString;
+
+        Scanner input = new Scanner(new File(currentFile));
+        text.clear();
+
+        while(input.hasNextLine()){
+            currentString = input.nextLine();
+            String[] words = currentString.split(" ");
+            for(var word : words){
+                text.add(word);
+            }
+        }
+    }
+}
