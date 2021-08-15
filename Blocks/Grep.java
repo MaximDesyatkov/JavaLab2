d29ac4915c888329ba9f2a0395d6e520e78b1437
MaximDesyatkov <78271197+MaximDesyatkov@users.noreply.github.com>
2021-08-15 17:03:42 +0700

    Update Grep.java

--- Blocks/Grep.java
+++ Blocks/Grep.java
@@ -1,1 +1,30 @@
+package blocks;
 
+import exception.*;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class Grep implements Operation {
+    public OperationTypes getType() {
+        return OperationTypes.IN_OUT;
+    }
+
+    public void execute(ArrayList<String> text, Vector<String> args) throws WrongNumberOfArgs {
+
+        if (args.size() != 1){
+            throw new WrongNumberOfArgs("Only 1 argument expected");
+        }
+
+        String word = args.get(0);
+
+        for (int i = 0; i < text.size(); i++) {
+            if (!text.get(i).contains(word)) {
+                text.remove(i);
+                i--;
+            }
+        }
+    }
+
+
+}
