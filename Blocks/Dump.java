70bc0edd217a0e7db2c0a4d92f3b68e2433c198a
MaximDesyatkov <78271197+MaximDesyatkov@users.noreply.github.com>
2021-08-15 17:02:54 +0700

    Update Dump.java

--- Blocks/Dump.java
+++ Blocks/Dump.java
@@ -1,1 +1,37 @@
+package blocks;
 
+import exception.*;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class Dump implements Operation {
+
+    public OperationTypes getType(){
+        return OperationTypes.IN_OUT;
+    }
+
+    public void execute(ArrayList<String> text, Vector<String> args) throws WrongNumberOfArgs, IOException {
+
+        if (args.size() != 1){
+            throw new WrongNumberOfArgs("Only 1 argument expected");
+        }
+
+        FileWriter fileWriter;
+        PrintWriter printWriter;
+
+        fileWriter = new FileWriter(args.get(0));
+        printWriter = new PrintWriter(fileWriter);
+
+        for (String word : text) {
+            printWriter.println(word);
+        }
+
+        fileWriter.close();
+        printWriter.close();
+    }
+
+}
