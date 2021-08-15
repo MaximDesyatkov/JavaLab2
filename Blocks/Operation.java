17c522c5f3ddf0b78b63b6096d62fca55ea11a9c
MaximDesyatkov <78271197+MaximDesyatkov@users.noreply.github.com>
2021-08-15 17:04:31 +0700

    Update Operation.java

--- Blocks/Operation.java
+++ Blocks/Operation.java
@@ -1,1 +1,10 @@
+package blocks;
 
+import java.util.*;
+
+
+public interface Operation {
+
+    void execute(ArrayList<String> text, Vector<String> args) throws Exception;
+    OperationTypes getType();
+}
